530805bcdf73392bededd3cbda6d679887f95442
mkaya13 <53873573+mkaya13@users.noreply.github.com>
2023-03-14 17:31:12 +0300

    We built migrations, models and we created the fundemantals of controllers and views for each table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :recipes
+  resources :recipe_foods
+  resources :foods
+  resources :inventory_foods
+  resources :inventories
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
