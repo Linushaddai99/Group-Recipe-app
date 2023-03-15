4097489a4692b96b9ad1970c2436fa9bd0fadda9
mkaya13 <53873573+mkaya13@users.noreply.github.com>
2023-03-15 13:22:10 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,18 @@ Rails.application.routes.draw do
   # resources :foods
   # resources :inventory_foods
   root "inventories#index"
+
+  # Routes for Inventory
   resources :inventories, only: [:new, :create, :edit, :index, :show, :destroy] do
 
   end
 
+  # Routes for Recipes
   resources :recipes, only: [:new, :create, :edit, :index, :show, :destroy] do
   
   end
+
+  # Routes for Is
 
   # resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
