d9389d987012e42b2aec8895c6ac413265c3029b
ndikumana isaie <ndikumanaisaie@gmail.com>
2023-03-15 23:22:15 +0200

    Add logic for the index action for public recipe controller

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,3 +1,8 @@
 class PublicRecipesController < ApplicationController
-  def index; end
+  def index
+    @public_recipes = Recipe.includes([:user], [:recipes_foods]).where(public: true).order(created_at: :desc)
+    @public_recipes.each do |recipe|
+      recipe.recipes_foods.all.includes([:food]).sort_by { |recipe_food| recipe_food.food.name }
+    end
+  end
 end
