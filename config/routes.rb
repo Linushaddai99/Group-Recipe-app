b06f6f97113cdc807c4510a1d80a23fbf3592398
Linus Shaddai <54178270+Linushaddai99@users.noreply.github.com>
2023-03-15 11:05:48 +0100

    Create routes for inventories and update seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  resources :recipes
-  resources :recipe_foods
-  resources :foods
-  resources :inventory_foods
-  resources :inventories
-  resources :users
+  # resources :recipes
+  # resources :recipe_foods
+  # resources :foods
+  # resources :inventory_foods
+  root "inventories#index"
+  resources :inventories, only: [:new, :create, :edit, :index, :show] do
+
+  end
+
+  # resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
