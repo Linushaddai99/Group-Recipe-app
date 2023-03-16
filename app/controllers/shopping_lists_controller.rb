fb562e061efbba501ee70f6670dd16cfe942a384
ndikumana isaie <ndikumanaisaie@gmail.com>
2023-03-16 20:36:33 +0200

    Fix rubocop offenses

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,9 +1,6 @@
 class ShoppingListsController < ApplicationController
   def index
-    # @foods = current_user.recipe_foods.select(:food_id, 'SUM(quantity) as quantity').group(:food_id, :quantity)
     @sum = 0
-    # recipe_foods = current_user.recipe_foods.select(:food_id, 'SUM(quantity) as quantity').group(:food_id, :quantity)
-    # inventory_foods = current_user.inventory_foods.select(:food_id, 'SUM(quantity) as quantity').group(:food_id, :quantity)
     missing_food_id = current_user.inventories.includes(:inventory_foods).pluck(:food_id) - current_user.recipes.includes(:recipe_foods).pluck(:food_id)
     @foods = InventoryFood.includes([:food]).where(food_id: missing_food_id)
     @foods.each do |food|
