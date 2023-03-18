805d3d0704e5d9bbd52b0f2f3d22efd747473629
ndikumana isaie <ndikumanaisaie@gmail.com>
2023-03-18 06:36:26 +0200

    Remove unused code

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -42,12 +42,6 @@ class FoodsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  # def set_food
-  #   @user = current_user
-  #   @food = Food.find(params[:id])
-  # end
-
   # Only allow a list of trusted parameters through.
   def food_params
     # params.fetch(:food, {})
